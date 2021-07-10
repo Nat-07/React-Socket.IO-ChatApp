064e9c665a0fa488756bfc8cc51516c5a0e58b67
Justin Morris <jnatmorris@gmail.com>
2021-07-10 11:46:25 -1000

    Compatible app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 import ChatMain from "./components/chat/chat";
 import Login from "./components/login/login";
-import { ThemeToggle } from "./components/darkMode";
 import AboutInfo from "./components/about/about";
+import Error from "./components/errorPage/errorPage";
+import { ThemeToggle } from "./components/darkMode";
 
 export default function App() {
+    const [isBase, setIsBase] = useState(true);
     return (
         <BrowserRouter>
-            <div className="flex justify-end py-3 dark: dark:bg-darkModeMain">
-                <ThemeToggle />
+            {/* Always top of page */}
+            <div className="flex justify-end py-2 pr-3 dark: dark:bg-darkModeMain">
+                <ThemeToggle isBase={isBase} setIsBase={setIsBase} />
             </div>
-            {/* main page */}
-            <Route exact path="/" component={Login} />
-            {/* redirect to chat-room form main*/}
-            <Route path="/chat" component={ChatMain} />
-            {/* redirect to about page from main */}
-            <Route path="/about" component={AboutInfo} />
+            <Switch>
+                {/* main page */}
+                <Route exact path="/" component={Login} />
+                {/* chat-room */}
+                <Route path="/chat" component={ChatMain} />
+                {/* about page */}
+                <Route path="/about" component={AboutInfo} />
+                <Route component={Error} />
+            </Switch>
         </BrowserRouter>
     );
 }

136e1c0d9c98cbb11abc619d6604a80c1ecbb4ad
Justin Morris <jnatmorris@gmail.com>
2021-07-19 16:08:47 -1000

    Add delivered notice

--- src/components/chat/helper.js
+++ src/components/chat/helper.js
@@ -8,12 +8,24 @@ function CurrentUsersData({ numCurrentUsers }) {
     return (
         <div className="grid col-span-1 pt-1 pl-2 justify-self-center h-min">
             <div className="inline-flex">
-                <p className="font-sans subpixel-antialiased">Online:&nbsp;</p>
+                <p className="font-sans subpixel-antialiased">Online:&nbsp; </p>
                 <p className="font-mono subpixel-antialiased">
                     {numCurrentUsers}
                 </p>
-
-                <span className="ml-2 animate-ping rounded-full h-1.5 w-1.5 bg-green-600"></span>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="ml-2 animate-customOnlinePing"
+                    height="10px"
+                    width="10px"
+                >
+                    <path
+                        fillRule="evenodd"
+                        d="M5.05 3.636a1 1 0 010 1.414 7 7 0 000 9.9 1 1 0 11-1.414 1.414 9 9 0 010-12.728 1 1 0 011.414 0zm9.9 0a1 1 0 011.414 0 9 9 0 010 12.728 1 1 0 11-1.414-1.414 7 7 0 000-9.9 1 1 0 010-1.414zM7.879 6.464a1 1 0 010 1.414 3 3 0 000 4.243 1 1 0 11-1.415 1.414 5 5 0 010-7.07 1 1 0 011.415 0zm4.242 0a1 1 0 011.415 0 5 5 0 010 7.072 1 1 0 01-1.415-1.415 3 3 0 000-4.242 1 1 0 010-1.415zM10 9a1 1 0 011 1v.01a1 1 0 11-2 0V10a1 1 0 011-1z"
+                        clipRule="evenodd"
+                    />
+                </svg>
             </div>
         </div>
     );

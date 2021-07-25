d3458f9fba6b45fc29bf417698b5cf1100e114f2
Justin Morris <jnatmorris@gmail.com>
2021-07-25 09:33:55 -1000

    Add animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 // tailwind.config.js
 module.exports = {
+    important: true,
     purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
     darkMode: "class", // or 'media' or 'class'
     theme: {
@@ -10,15 +11,60 @@ module.exports = {
                 darkModeHeadFoot: "#355069",
                 divideLine: "#27435c",
             },
-            // custom z values
-            zIndex: {
-                // header in chat
-                500: "500",
-                // theme toggle
-                999: "999",
+
+            opacity: {
+                91: ".91",
+                99: ".99",
             },
-            // Animation for dark/light icon
+
+            // https://devdojo.com/tnylea/custom-animations-in-tailwindcss
+            keyframes: {
+                "fade-in-down": {
+                    "0%": {
+                        opacity: "0",
+                        transform: "translateY(-10px)",
+                    },
+                    "100%": {
+                        opacity: "1",
+                        transform: "translateY(0)",
+                    },
+                },
+                "fade-out-down": {
+                    from: {
+                        opacity: "1",
+                        transform: "translateY(0px)",
+                    },
+                    to: {
+                        opacity: "0",
+                        transform: "translateY(10px)",
+                    },
+                },
+                "fade-in-up": {
+                    "0%": {
+                        opacity: "0",
+                        transform: "translateY(10px)",
+                    },
+                    "100%": {
+                        opacity: "1",
+                        transform: "translateY(0)",
+                    },
+                },
+                "fade-out-up": {
+                    from: {
+                        opacity: "1",
+                        transform: "translateY(0px)",
+                    },
+                    to: {
+                        opacity: "0",
+                        transform: "translateY(10px)",
+                    },
+                },
+            },
             animation: {
+                "fade-in-down": "fade-in-down 0.5s ease-out",
+                "fade-out-down": "fade-out-down 0.5s ease-out",
+                "fade-in-up": "fade-in-up 0.5s ease-out",
+                "fade-out-up": "fade-out-up 0.5s ease-out",
                 lightDarkToggle: "spin 1s",
                 customOnlinePing: "ping 1.25s infinite",
                 customMyMessagePulse: "pulse 1.5s",
@@ -32,6 +78,7 @@ module.exports = {
             animation: ["disabled"],
             ringWidth: ["disabled"],
             ringOffsetWidth: ["disabled"],
+            opacity: ["dark"],
         },
     },
     plugins: [],
